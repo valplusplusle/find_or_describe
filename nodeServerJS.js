ceb9748a7699e30c8c68c335a3005f6632018494
admin <blank@h2808199.stratoserver.net>
2019-09-10 15:59:59 +0200

    implement logic

--- nodeServerJS.js
+++ nodeServerJS.js
@@ -1,10 +1,21 @@
-var express = require('express');
+var app = express();
+app.use(express.bodyParser());
 var app = express();
 app.use(express.bodyParser());
 
+var https = require('https');
+var fs = require('fs');
+
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "*");
+  next();
+}
+app.use(allowCrossDomain);
+
 counterFileName = 0;
 
-app.post('/', function (req, res) {
+app.post('/send', function (req, res) {
   test = JSON.stringify(req.body);
   console.log(test);      // your JSON
   const fs = require('fs');
@@ -17,6 +28,21 @@ app.post('/', function (req, res) {
     });
     counterFileName = counterFileName + 1;
 });
+
+app.get('/get', function (req, res) {
+  randomObject = Math.floor(Math.random() * 5);
+  console.log(randomObject);
+  var fs = require("fs");
+  fs.readFile(randomObject.toString(), function(err, buf) {
+        test=JSON.parse(buf.toString());
+        res.send(test);
+  });
+});
+
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}, app)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
