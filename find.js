ded36c227bc296f715fc5b9c427d88d0f73d4a1a
admin <blank@h2808199.stratoserver.net>
2019-09-11 14:19:20 +0200

    testing if pressed picture ist the right one

--- find.js
+++ find.js
@@ -1,11 +1,23 @@
-function getObject(){
+var rightLink= "";
+var wrongLink= "";
+
+function getObject() {
 	fetch('http://ide.blank-42.de:3000/get')
     .then(response => response.json())
     .then(data => {
     	console.log(data);
-        document.getElementById("output").innerHTML = "<img src='"+ data["picture"] +"' class='img-fluid'></img>";
-        document.getElementById("output").innerHTML += "<img src='"+ data["pictureWrong"] +"' class='img-fluid'></img>";
+    	rightLink = data["picture"];
+    	wrongLink = data["pictureWrong"]
+        console.log(rightLink);
+        document.getElementById("output").innerHTML = "<img onClick='check(rightLink)' src='"+ data["picture"] +"' class='img-fluid'></img>";
+        document.getElementById("output").innerHTML += "<img onClick='check(wrongLink)' src='"+ data["pictureWrong"] +"' class='img-fluid'></img>";
         document.getElementById("describe").innerHTML = data["text"];
-        rightLink = data["picture"];
+
     });
+}
+
+function check(link) {
+	if(link == rightLink){alert("Juhuu");}
+	else {alert("buhh");}
+
 }
