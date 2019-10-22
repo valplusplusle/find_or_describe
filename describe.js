2a51c34e042d65b565e487199e5e317c1ce05569
admin <blank@h2808199.stratoserver.net>
2019-10-22 17:25:58 +0200

    Fixing bugs

--- describe.js
+++ describe.js
@@ -1,4 +1,5 @@
-var pictureLink;
+var pictureLink="";
+var wrongPictureLink="";
 
 function randomNumber(maxNumber) {
 	return Math.floor(Math.random() * maxNumber) + 1;
@@ -8,26 +9,26 @@ function describe() {
 	fetch('https://pixabay.com/api/?key=7701663-a26ee9d1fce25b3e9eafcdea9&per_page=200&image_type=photo')
     .then(response => response.json())
     .then(data => {
-    	$pictureLink = data["hits"][randomNumber(200)]["webformatURL"];
-    	$wrongPictureLink = data["hits"][randomNumber(200)]["webformatURL"];
-        document.getElementById("output").innerHTML = "<img src='"+ $pictureLink +"' class='img-fluid rounded mx-auto d-block'></img>";
+    	pictureLink = data["hits"][randomNumber(200)]["webformatURL"];
+    	wrongPictureLink = data["hits"][randomNumber(200)]["webformatURL"];
+        document.getElementById("output").innerHTML = "<img src='"+ pictureLink +"' class='img-fluid rounded mx-auto d-block'></img>";
         send()
     });
 }
 
 function send() {
-	document.getElementById("describe").innerHTML = '<form onSubmit="sendToServer()" class="d-flex justify-content-center"> <label for="name">Describe:</label> <input type="text" id="text" maxlength="10" name="text" /> <input type="submit" value="GO!" onClick="describe()"/> <form>';
+	document.getElementById("describe").innerHTML = '<form class="d-flex justify-content-center"> <label for="name">Describe:</label> <input type="text" id="text" maxlength="10" name="text" /> <button onClick="sendToServer()">Send</button> <form>';
 }
 
 function sendToServer(){
 	console.log("test");
-
-	fetch('http://ide.blank-42.de:3000/get', {
+    text = document.getElementById("text").value;
+	fetch('https://ide.blank-42.de:3000/send', {
 		headers: { "Content-Type": "application/json; charset=utf-8" },
 		method: 'POST',
 		body: JSON.stringify({
-			text: Textinputfeldvaluehere,
-			picture: picutreLink,
+			text: text,
+			picture: pictureLink,
 			pictureWrong: wrongPictureLink
 		})
 	})
